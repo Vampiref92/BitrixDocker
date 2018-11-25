0935f859c1a46c142a902c3591c1f9c1f5f97723
Sergey Frolov <frolov@adv.ru>
2018-11-26 00:24:54 +0300

    update dockerfile and settings

--- restore_mysql.sh
+++ restore_mysql.sh
@@ -6,5 +6,19 @@ PATH_TO_BACKUP="/home/frolov/bitrixFiles/toplivnye-karty/bitrix.sql"
 
 . ./.env
 
+# проверяем есть ли БД, если нет то создаем
+DB="$(docker exec -i ${MYSQL_CONTAINER_NAME} mysql -uroot -e 'SHOW DATABASES')"
+if(  ${DB} =~ ${MYSQL_DATABASE}); then
+    PASSWORD_S="";
+    if( ! -n ${MYSQL_ROOT_PASSWORD}); then
+        PASSWORD_S=" -p${MYSQL_ROOT_PASSWORD}"
+    fi
+    CREATE DATABASE ${MYSQL_DATABASE} | docker exec -i ${MYSQL_CONTAINER_NAME} mysql -uroot ${PASSWORD_S}
+    CREATE USER ${MYSQL_USER}@'localhost' IDENTIFIED BY ${MYSQL_PASSWORD}
+    GRANT ALL PRIVILEGES ON * . * TO ${MYSQL_USER}@'localhost';
+    FLUSH PRIVILEGES;
+fi;
+
 #стартуем докер
 cat ${PATH_TO_BACKUP} | docker exec -i ${MYSQL_CONTAINER_NAME} mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}
+#cat ${PATH_TO_BACKUP} | docker exec -i ${MYSQL_CONTAINER_NAME} mysql -uroot ${MYSQL_DATABASE}
--- start.sh
+++ start.sh
@@ -11,4 +11,5 @@ sudo ip a add ${INTERFACE}/24 dev lo
 addhost ${DOMAIN} ${INTERFACE}
 
 #стартуем докер
+# docker network prune
 docker-compose up --build --remove-orphans -d ; docker-compose ps
