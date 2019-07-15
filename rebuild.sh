f004755a17c51e682e254da8f9154fc0f687f580
frolov <sergeyfolov@inetsys.ru>
2019-07-15 12:42:39 +0300

    [master] up

--- rebuild.sh
+++ rebuild.sh
@@ -2,10 +2,25 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#останавливаем все контейнеры
-docker stop $(docker ps -a -q)
-
 cd ${DIR}
+
+. ./.env
+. ./manage-etc-hosts.sh
+
+docker-compose stop
+
+removehost ${DOMAIN}
+sudo ip a delete ${INTERFACE}/24 dev lo
+
+# стопаем все запущенные контейнеры
+docker stop $(docker ps -a -q)
+# удаляем все контейнеры
+docker rm $(docker ps -a -q)
+# удаляем все нетворки
+docker network prune -f
+
+sudo ip a add ${INTERFACE}/24 dev lo
+addhost ${DOMAIN} ${INTERFACE}
 
 #стартуем докер
 docker-compose up --build --remove-orphans --force-recreate -d ; docker-compose ps
--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
+. ./.env
+. ./manage-etc-hosts.sh
+
 # стопаем все запущенные контейнеры
 docker stop $(docker ps -a -q)
 # удаляем все контейнеры
 docker rm $(docker ps -a -q)
 # удаляем все нетворки
 docker network prune -f
-
-. ./.env
-. ./manage-etc-hosts.sh
 
 sudo ip a add ${INTERFACE}/24 dev lo
 addhost ${DOMAIN} ${INTERFACE}
